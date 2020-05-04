d4af3a736391574d6a243331005ad7d5bb425590
Zishan Ansari <zshan.ansari07@gmail.com>
2020-05-04 17:53:30 +0530

    Updated Functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,37 @@ toggleTabs();
 
 // Function for toggling accordion
 function toggleAccordion() {
-  var accordions = document.getElementsByClassName("accordion-title");
+  var accordions = document.getElementsByClassName('accordion-title');
+  var accordionsData = document.getElementsByClassName('accordion-data');
   var accordionsLength = accordions.length;
 
   for (var i = 0; i < accordionsLength; i++) {
     accordions[i].onclick = function () {
-      this.classList.toggle('active');
-      this.firstElementChild.classList.toggle('active');
       var content = this.nextElementSibling;
       // accordion is currently open, so close it
       if (content.style.maxHeight) {
         content.style.maxHeight = null;
+        this.classList.remove('active');
+        this.firstElementChild.classList.remove('active');
       } else { // accordion is currently closed, so open it
-        content.style.maxHeight = content.scrollHeight + "px";
+        content.style.maxHeight = content.scrollHeight + 'px';
+        this.classList.add('active');
+        this.firstElementChild.classList.add('active');
       }
+
+      // Add/Remove codes below this line for single/multiple opened accordions
+      for (var i = 0; i < accordionsLength; i++) {
+        if (accordionsData[i].style.maxHeight) {
+          // This will remove all the properties
+          accordionsData[i].style.maxHeight = null;
+          accordions[i].classList.remove('active');
+          accordions[i].firstElementChild.classList.remove('active');
+          // This will add the properties on clicked accordion
+          content.style.maxHeight = content.scrollHeight + 'px';
+          this.classList.add('active');
+          this.firstElementChild.classList.add('active');
+        }
+      } // Remove till here if you want multiple accordion
     }
   }
 }
@@ -39,18 +56,18 @@ function toggleTabs() {
     }
     // Gets clicked tab
     var clickedTab = tabClickEvent.currentTarget;
-    clickedTab.classList.add("active"); // Sets active class on clicked tab
+    clickedTab.classList.add('active'); // Sets active class on clicked tab
     tabClickEvent.preventDefault();
-    var contentData = document.querySelectorAll(".tab-data");
+    var contentData = document.querySelectorAll('.tab-data');
     var contentDataLength = contentData.length;
     // Removes all the active class present
     for (var i = 0; i < contentDataLength; i++) {
-      contentData[i].classList.remove("active");
+      contentData[i].classList.remove('active');
     }
     var anchorReference = tabClickEvent.target;
-    var dataId = anchorReference.getAttribute("href");
+    var dataId = anchorReference.getAttribute('href');
     var activeData = document.querySelector(dataId);
     // Adds class active on clicked Id
-    activeData.classList.add("active");
+    activeData.classList.add('active');
   }
 }
